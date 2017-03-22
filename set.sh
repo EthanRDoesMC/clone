151b2e9c66a2e6f315a095a9b1e5cae1a95dc045
EthanRDoesMC <ethanrdoesmc@gmail.com>
2017-03-22 17:05:22 -0400

    Updated

--- set.sh
+++ set.sh
@@ -8,16 +8,4 @@ git clone "https://github.com/EthanRDoesMC/manager10.git" ethanr/manager10
 
 git clone "https://github.com/EthanRDoesMC/ethanrdoesmc.github.io.git" ethanr/site
 
-git clone "https://github.com/EthanRDoesMC/managerwebdemo.git" ethanr/managerwebdemo
-
 echo "Finished cloning."
-
-sleep 15
-
-echo "Done staring at the terminal?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) exit;;
-        No ) echo "Too bad, it's closing anyway."; sleep 5; exit;;
-    esac
-done
