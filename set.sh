aeb5a0909ad82fb0e6d0803ec69aff59cdd28c21
EthanRDoesMC <EthanRDoesMC@users.noreply.github.com>
2017-03-23 12:08:42 -0400

    Update set.sh

--- set.sh
+++ set.sh
@@ -8,4 +8,6 @@ git clone "https://github.com/EthanRDoesMC/manager10.git" ethanr/manager10
 
 git clone "https://github.com/EthanRDoesMC/ethanrdoesmc.github.io.git" ethanr/site
 
+git clone "https://github.com/ethanrdoesmc/breadcrumbarrows.git
+
 echo "Finished cloning."
